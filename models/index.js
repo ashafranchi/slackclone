3a04c38785302fa940191061d431748acf85c8b3
Asha <asha17@brown.edu>
2020-11-09 16:40:32 -0800

    start

---
+++ models/index.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize(config.database, config.username, config.password);
+
+const models = {
+    user: sequelize.import("./users"),
+};
+
+Object.keys(db).forEach((modelName) => {
+    if ("associate" in db[modelName]) {
+        db[ModelName].associate(db);
+    }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+
+export default models;
